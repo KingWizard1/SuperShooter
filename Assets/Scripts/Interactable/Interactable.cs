f4136341b238dffaf5b41e14a5f77a7b0ed443d3
perfectdark420 <47585882+perfectdark420@users.noreply.github.com>
2019-03-21 16:17:24 +1100

    Fixed Probuilder objects not rotating because they were fucking marked as STATIC you idiot >.<

--- Assets/Scripts/Interactable/Interactable.cs
+++ Assets/Scripts/Interactable/Interactable.cs
@@ -4,8 +4,22 @@ using UnityEngine;
 
 namespace SuperShooter
 {
+    public enum InteractableType
+    {
+        Weapon,
+        Gadget,
+
+    }
+
+    // ----------------------------------------------------- //
+
     public class Interactable : MonoBehaviour
     {
+
+        public InteractableType Type;
+
+        // ------------------------------------------------- //
+
 
         public virtual void Interact()
         {

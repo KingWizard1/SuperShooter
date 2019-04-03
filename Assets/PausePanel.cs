fa56785e544bfda472e758ce5645fcdcd46c6099
threepat1 <42788704+threepat1@users.noreply.github.com>
2019-04-04 01:08:40 +1100

    add key panel and switch script

--- Assets/PausePanel.cs
+++ Assets/PausePanel.cs
@@ -9,18 +9,59 @@ namespace SuperShooter
     {
         public GameObject soundButton, systemButton, keyButton;
         public GameObject soundPanel, systemPanel, keyPanel;
-        public Image soundImage1, soundImage2, systemImage1, systemImage2, keyImage1, keyImage2;
+        public GameObject soundImage1,systemImage1, keyImage1;
+        public GameObject soundText1, soundText2, systemText1, systemText2, keyText1, keyText2;
         // Use this for initialization
         void Start()
         {
             soundButton = GetComponent<GameObject>();
             systemButton = GetComponent<GameObject>();
+            keyButton = GetComponent<GameObject>();
+            soundPanel = GetComponent<GameObject>();
+            systemPanel = GetComponent<GameObject>();
+            keyPanel = GetComponent<GameObject>();
+            soundImage1 = GetComponent<GameObject>();
+            systemImage1 = GetComponent<GameObject>();
+            keyImage1 = GetComponent<GameObject>();
+            soundText1 = GetComponent<GameObject>();
+            soundText2 = GetComponent<GameObject>();
+            systemText1 = GetComponent<GameObject>();
+            systemText2 = GetComponent<GameObject>();
+            keyText1 = GetComponent<GameObject>();
+            keyText2 = GetComponent<GameObject>();
+            systemPanel.SetActive(false);
+            keyPanel.SetActive(false);
         }
 
         // Update is called once per frame
         void Update()
         {
 
+        }
+        public void ClickSoundPanel()
+        {
+            soundPanel.SetActive(true);
+            soundImage1.SetActive(true);
+            soundText2.SetActive(true);
+            soundText1.SetActive(false);
+            systemText2.SetActive(false);
+            keyText2.SetActive(false);
+            systemPanel.SetActive(false);
+            keyPanel.SetActive(false);
+            systemImage1.SetActive(false);
+            keyImage1.SetActive(false);
+        }
+        public void ClickSystemPanel()
+        {
+            systemPanel.SetActive(true);
+            systemImage1.SetActive(true);
+            systemText2.SetActive(true);
+            soundText1.SetActive(true);
+            soundText2.SetActive(false);
+            systemText1.SetActive(false);
+            keyText2.SetActive(false);
+            keyPanel.SetActive(false);           
+            keyImage1.SetActive(false);
         }
     }
 }
--- Assets/UI/Script/PauseMenu.cs
+++ Assets/UI/Script/PauseMenu.cs
@@ -7,126 +7,133 @@ using UnityEngine.UI;
 namespace superShooter
 {
 
-
-public class PauseMenu : MonoBehaviour
-{
-
-    public static bool paused;
-    public bool showOption;
-    public GameObject pauseMenu, optionMenu;
-    public AudioSource soundAudio;
-    public Light dirLight;
-    public Slider soundSlider;
-    public Slider lightSlider;
-
-
-
-    // Use this for initialization
-    void Start()
+    public class PauseMenu : MonoBehaviour
     {
 
-        Time.timeScale = 1f;
-        paused = false;
-        pauseMenu.SetActive(false);
+        public static bool paused;
+        public bool showOption;
+        public GameObject pauseMenu;
+        public AudioSource soundAudio;
+        public Light dirLight;
+        public Slider soundSlider;
+        public Slider lightSlider;
+
+        public GameObject soundButton, systemButton, keyButton;
+        public GameObject soundPanel, systemPanel, keyPanel;
+        public GameObject soundImage1, systemImage1, keyImage1;
+        //public GameObject soundText1, soundText2, systemText1, systemText2, keyText1, keyText2;
 
 
-        soundAudio = GameObject.Find("Audio Source").GetComponent<AudioSource>();
-        dirLight = GameObject.Find("Directional Light").GetComponent<Light>();
-        soundSlider.value = PlayerPrefs.GetFloat("Audio Source");
-        lightSlider.value = PlayerPrefs.GetFloat("Directional Light");
-        return;
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        // Use this for initialization
+        void Start()
         {
-            TogglePause();
-        }
-    }
-    public void ToggleOption()
-    {
-        if (showOption)
-        {
-            showOption = false;
-            pauseMenu.SetActive(true);
-            optionMenu.SetActive(false);
 
-
-        }
-        else
-        {
-            showOption = true;
+            Time.timeScale = 1f;
+            paused = false;
             pauseMenu.SetActive(false);
-            optionMenu.SetActive(true);
-            Cursor.lockState = CursorLockMode.None;
-            Cursor.visible = true;
-            soundSlider = GameObject.Find("AudioSlider").GetComponent<Slider>();
-            lightSlider = GameObject.Find("Brightness").GetComponent<Slider>();
-            soundSlider.value = soundAudio.volume;
-            lightSlider.value = dirLight.intensity;
 
+
+            soundAudio = GameObject.Find("Audio Source").GetComponent<AudioSource>();
+            dirLight = GameObject.Find("Directional Light").GetComponent<Light>();
+            soundSlider.value = PlayerPrefs.GetFloat("Audio Source");
+            lightSlider.value = PlayerPrefs.GetFloat("Directional Light");
+            return;
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Time.timeScale = 0;
+                pauseMenu.SetActive(true);
+                paused = true;
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+
+                systemPanel.SetActive(false);
+                systemImage1.SetActive(false);
+
+                soundPanel.SetActive(true);
+                soundImage1.SetActive(true);
+
+                keyPanel.SetActive(false);
+                keyImage1.SetActive(false);
+                OnClickSystemPanel();
+                OnClickSoundPanel();
+            }
+        }
+ 
+        public void Resume()
+        {
+            paused = false;
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1f;
+
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
 
 
         }
-        optionMenu.SetActive(showOption);
-    }
-    public void Resume()
-    {
-        paused = false;
-        pauseMenu.SetActive(false);
-        Time.timeScale = 1f;
-
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-
-
-    }
-    public void LoadMenu()
-    {
-        Time.timeScale = 1f;
-        PlayerPrefs.SetFloat("Audio Source", soundAudio.volume);
-        PlayerPrefs.SetFloat("Directional Light", dirLight.intensity);
-        SceneManager.LoadScene(0);
-    }
-    public void Exitmenu()
-    {
+        public void LoadMenu()
+        {
+            Time.timeScale = 1f;
+            PlayerPrefs.SetFloat("Audio Source", soundAudio.volume);
+            PlayerPrefs.SetFloat("Directional Light", dirLight.intensity);
+            SceneManager.LoadScene(0);
+        }
+        public void Exitmenu()
+        {
 #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
+            UnityEditor.EditorApplication.isPlaying = false;
 #endif
-        Application.Quit();
-    }
-    public void TogglePause()
-    {
+            Application.Quit();
+        }
 
-        Time.timeScale = 0;
-        pauseMenu.SetActive(true);
-        paused = true;
-        Cursor.lockState = CursorLockMode.None;
-        Cursor.visible = true;
-        //player.GetComponent<CharacterMovement>().enabled = false;
-        //player.GetComponent<MouseLook>().enabled = false;
-        //mainCam.GetComponent<MouseLook>().enabled = false;
+        public void Volume()
+        {
+            soundAudio.volume = soundSlider.value;
+        }
+        public void Brightness()
+        {
+            dirLight.intensity = lightSlider.value;
 
+        }
+        public void OnClickSystemPanel()
+        {
+            systemPanel.SetActive(true);
+            systemImage1.SetActive(true);
 
-    }
-    public void BackMenu()
-    {
-        showOption = false;
-        pauseMenu.SetActive(true);
-        optionMenu.SetActive(false);
-        return;
-    }
-    public void Volume()
-    {
-        soundAudio.volume = soundSlider.value;
-    }
-    public void Brightness()
-    {
-        dirLight.intensity = lightSlider.value;
+            soundPanel.SetActive(false);
+            soundImage1.SetActive(false);
 
+            keyPanel.SetActive(false);
+            keyImage1.SetActive(false);
+        }
+        public void OnClickSoundPanel()
+        {
+            systemPanel.SetActive(false);
+            systemImage1.SetActive(false);
+
+            soundPanel.SetActive(true);
+            soundImage1.SetActive(true);
+
+            keyPanel.SetActive(false);
+            keyImage1.SetActive(false);
+        }
+        public void OnclickKeyPanel()
+        {
+
+                systemPanel.SetActive(false);
+                systemImage1.SetActive(false);
+
+                soundPanel.SetActive(false);
+                soundImage1.SetActive(false);
+
+                keyPanel.SetActive(true);
+                keyImage1.SetActive(true);
+            
+        }
     }
-}
 }
